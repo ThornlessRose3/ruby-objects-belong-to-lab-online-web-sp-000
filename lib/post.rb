6c889d273c908ffc2a43c5787802104b5f694b49
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-01 18:42:19 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,9 @@
 class Post
   
-  attr_accessor :title, :author
+  attr_accessor :title
   
-  def initialize(name, author)
+  def initialize(name)
     @title = title
-    @author = author
   end
   
 end
